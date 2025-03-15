909c1ba62d1d836177546bd8c7ab0b451bdad2cb
Herman Cortez <herman.cortez@gmail.com>
2025-03-15 14:42:52 -0300

    feat: proyecto inicial .NET 8 con arquitectura limpia

---
+++ WalletAPI/Domain/Entities/Transaction.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int WalletId { get; set; }
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Wallet Wallet { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Debit,
+        Credit
+    }
+}
---
+++ WalletAPI/Domain/Entities/Wallet.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public string DocumentId { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+}
